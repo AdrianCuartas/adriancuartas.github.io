56130fe1a253ba1c30e817bcda7061501f403b51
Adrian Cuartas <adrian.cuartas@protonmail.com>
2018-03-17 13:24:58 +0100

    fix: Change default port

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -7,9 +7,8 @@ class DBHelper {
    * Database URL.
    * Change this to restaurants.json file location on your server.
    */
-  static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+  static get DATABASE_URL() {   
+    return `http://${window.location.host}/data/restaurants.json`;
   }
 
   /**
